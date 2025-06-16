06c9bbcc0f4c283a0458fc55ec53dcf843a066f8
Huy Le <legiahuy050104@gmail.com>
2025-06-16 16:43:15 -0400

    index.tsx created

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+/**
+ * frontend/src/pages/index.tsx
+ *
+ * Renders the homepage containing the IngestForm.
+ */
+
+import IngestForm from "../components/IngestForm";
+
+export default function Home() {
+  return (
+    <div>
+      <IngestForm />
+    </div>
+  );
+}
