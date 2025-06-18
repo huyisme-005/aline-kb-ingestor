1507cc97cc600b3bc1e7e0c2bf86b65c925eca47
Huy Le <legiahuy050104@gmail.com>
2025-06-17 22:53:33 -0400

    main.py updated

--- backend/api/main.py
+++ backend/api/main.py
@@ -234,4 +234,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.main:app", host="localhost", port=8000, reload=True)
