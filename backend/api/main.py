13e10f6b287d1394b31c842b7842cdd7f2c5fa8f
Huy Le <legiahuy050104@gmail.com>
2025-06-18 11:25:09 -0400

    main.py updated

--- backend/api/main.py
+++ backend/api/main.py
@@ -39,6 +39,8 @@ app.add_middleware(
     allow_origins=[
         "http://0.0.0.0:3000", 
         "http://127.0.0.1:3000",
+        "http://localhost:3000",
+        "http://localhost:8000",
         "http://0.0.0.0:3001", 
         "http://127.0.0.1:3001",
         "http://0.0.0.0:8080", 
