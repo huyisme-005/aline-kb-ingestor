e90afb996fa72e96a0e9e5b8cb192de7df406cbc
Huy Le <legiahuy050104@gmail.com>
2025-06-19 17:43:24 -0400

    serverless.yml and next.config.js updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,4 +2,15 @@
 module.exports = {
   reactStrictMode: true,
   output: 'export',
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://61q69bcvs0.execute-api.us-east-1.amazonaws.com/dev'
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://61q69bcvs0.execute-api.us-east-1.amazonaws.com/dev/:path*'
+      }
+    ]
+  }
 };
